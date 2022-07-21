4bffaf63dc4433a48cb94273f366fbbf437b69fd
suckitfa <254202042@qq.com>
2022-07-21 15:17:51 +0800

    WIP: nested router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import GoodsList from '@/components/GoodsList'
+import GoodsList from '@/views/GoodsList'
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     },{
-      path:"/goods/:goodsId",
+      path:"/goods/:goodsId/admin/:name",
       name:"GoodsList",
       component:GoodsList
-    }
+    },
+    // 嵌套路由
   ]
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+     <div>这是商品页面</div>
+     <p>商品序号：{{$route.params.goodsId}}</p>
+     <p>管理员名字：{{$route.params.name}}</p>
+   </div>
+</template>
+<script>
+export default {
+    name:"GoodsList",
+    mounted() {
+        console.log(this.$route.params)
+    }
+}
+</script>
