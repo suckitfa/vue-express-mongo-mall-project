428d2503124c03fe12cd18bd04ddf713476687f6
suckitfa <254202042@qq.com>
2022-07-29 10:33:14 +0800

    feat: 使用事件代理优化模态框组件

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,7 +5,7 @@
     <div class="md-modal-inner">
         <div class="md-top">
             <div class="md-title">提示</div>
-            <button class="md-close" @click="closeModal">Close</button>
+            <button class="md-close">Close</button>
         </div>
         <div class="md-content">
             <div class="confirm-tips">
@@ -17,7 +17,7 @@
         </div>
     </div>
 </div> 
-<div class="md-overlay" v-show="mdShow" @click="closeModal"></div>
+<div class="md-overlay" v-show="mdShow" ></div>
  </div>
  </template>
 
@@ -29,7 +29,8 @@
         }
     },
     methods: {
-        closeModal() {
+        closeModal(e) {
+            console.log(e)
             this.$emit('closeModalEvent')
         }
     }
